66041b88517e02e512fb0c62f9628571f81cdb28
luckac69 <luckac69@gmail.com>
2024-11-22 23:59:05 -0500

    gun

---
+++ bullet.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+
+const SPEED: int = 300
+
+
+func _process(delta: float) -> void:
+	position += transform.x * SPEED * delta
--- gun.gd
+++ gun.gd
@@ -1,4 +1,23 @@
 extends Node2D
 
+
+const BULLET = preload("res://Bullet.tscn")
+
+
 func _process(delta: float) -> void:
+
+	look_at(get_global_mouse_position())
+
+	rotation_degrees = wrap(rotation_degrees, 0, 360)
+
+	if rotation_degrees > 90 and rotation_degrees < 270:
+		scale.y = -.2
+	else:
+		scale.y = .2
+
+	if Input.is_action_just_pressed("Fire"):
+		var bullet_instance = BULLET.instantiate()
+		get_tree().root.add_child(bullet_instance)
+		bullet_instance.global_position = global_position
+		bullet_instance.rotation = rotation
 	
