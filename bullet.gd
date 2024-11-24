113534fa9e6fe4f9dabd94fd18b99bb4c68ed5cc
luckac69 <luckac69@gmail.com>
2024-11-24 17:09:06 -0500

    tileMap2

--- bullet.gd
+++ bullet.gd
@@ -3,11 +3,20 @@ extends Area2D
 
 const SPEED: int = 800
 
-	
+var out_of_Shooter: bool = false
 
 func _process(delta: float) -> void:
+	z_index = -1
 	position += transform.x * SPEED * delta
 
 
 func _on_kill_timeout() -> void:
-	self.get_parent().remove_child(self)
+	queue_free()
+
+
+
+func _on_body_entered(body:Node2D) -> void:
+	if out_of_Shooter:
+		queue_free()
+
+
