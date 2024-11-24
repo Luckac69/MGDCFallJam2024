ff0af33534ea93dea5334fc4f4ce1c0ce71703e0
luckac69 <luckac69@gmail.com>
2024-11-23 20:12:06 -0500

    gun 2

--- bullet.gd
+++ bullet.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
 
-const SPEED: int = 300
+const SPEED: int = 800
 
+	
 
 func _process(delta: float) -> void:
 	position += transform.x * SPEED * delta
+
+
+func _on_kill_timeout() -> void:
+	self.get_parent().remove_child(self)
